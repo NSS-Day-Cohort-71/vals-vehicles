a516dbe86ee5a7744427e7ceb774f87378f2d826
Steve Brownlee <chortlehoort@gmail.com>
2024-08-16 10:41:35 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Import all package members here
+
+
+
+# Create instances of vehicles to be stored here
+
+
+
+# Create instances of storage facilities here
+
+
+
+# Store vehicles in facilities here
+
+
+
+
